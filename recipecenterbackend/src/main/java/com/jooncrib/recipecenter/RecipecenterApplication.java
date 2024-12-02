e7392050a416e61bc685750d2a8fde0c438e6db4
Joon Choi <joon33103@gmail.com>
2024-12-02 12:11:40 -0800

    first check in

---
+++ recipecenterbackend/src/main/java/com/jooncrib/recipecenter/RecipecenterApplication.java
@@ -0,0 +1,13 @@
+package com.jooncrib.recipecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipecenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipecenterApplication.class, args);
+	}
+
+}
---
+++ recipecenterbackend/src/test/java/com/jooncrib/recipecenter/RecipecenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jooncrib.recipecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipecenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
